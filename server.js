62c9ebac3f7c4196b4f51db234e7634ab5ca5738
Devan Ash <141689897+DAsh365@users.noreply.github.com>
2024-02-11 12:38:30 -0700

    updated files

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,6 +8,10 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+const generateUniqueId = () => {
+    return '_' + Math.random().toString(36).substr(2, 9);
+};
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
@@ -26,7 +29,7 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.post('/api/notes', (req, res) => {
-    const newNote = { ...req.body, id: uuidv4() };
+    const newNote = { ...req.body, id: generateUniqueId() };
 
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if (err) throw err;
